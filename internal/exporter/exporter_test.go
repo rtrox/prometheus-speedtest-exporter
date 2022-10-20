8522c09499d4a2668d53078a2063e4a12cfde162
Russell Troxel <russelltroxel@gmail.com>
2022-10-19 18:07:41 -0700

    Initial commit

---
+++ internal/exporter/exporter_test.go
@@ -0,0 +1,128 @@
+package exporter
+
+import (
+	"context"
+	"io"
+	"net/http/httptest"
+	"regexp"
+	"testing"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	dto "github.com/prometheus/client_model/go"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"github.com/stretchr/testify/require"
+	"github.com/tj/assert"
+)
+
+func init() {
+	log.Logger = zerolog.New(io.Discard)
+}
+
+func TestDescribe(t *testing.T) {
+	assert := assert.New(t)
+	e := New(Opts{})
+
+	ch := make(chan *prometheus.Desc)
+	received := 0
+	go func() {
+		assert.NotPanics(func() {
+			e.Describe(ch)
+		})
+		close(ch)
+	}()
+	for elem := range ch {
+		assert.NotEqual(&prometheus.Desc{}, elem)
+		received++
+	}
+	assert.GreaterOrEqual(received, 5)
+}
+
+func TestCollect(t *testing.T) {
+	assert := assert.New(t)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+	e := New(Opts{
+		Doer: NewTestClient(),
+		Ctx:  ctx,
+	})
+	ch := make(chan prometheus.Metric)
+	received := 0
+	go func() {
+		assert.NotPanics(func() {
+			e.Collect(ch)
+		})
+		close(ch)
+	}()
+
+	for elem := range ch {
+		metric := &dto.Metric{}
+		elem.Write(metric)
+
+		assert.NotEqual(0, metric.GetGauge().GetValue())
+		received++
+	}
+	assert.GreaterOrEqual(received, 5)
+
+}
+
+/* Example Output:
+# HELP speedtest_download_speed_mbps Latency to Speedtest Server in seconds
+# TYPE speedtest_download_speed_mbps gauge
+speedtest_download_speed_mbps{country="United States",distance="1.0",lat="1.0",lon="-1.0",name="Anytown, USA",server_id="1",sponsor="Dat Sponsor Doh",url="http://speedtest.example.net:8080/speedtest/upload.php"} 716.7810615213976
+# HELP speedtest_exporter_info Info about this speedtest-exporter
+# TYPE speedtest_exporter_info gauge
+speedtest_exporter_info{app_name="speedtest-exporter",app_version="x.x.x"} 1
+# HELP speedtest_latency_ms Latency to Speedtest Server in seconds
+# TYPE speedtest_latency_ms gauge
+speedtest_latency_ms{country="United States",distance="1.0",lat="1.0",lon="-1.0",name="Anytown, USA",server_id="1",sponsor="Dat Sponsor Doh",url="http://speedtest.example.net:8080/speedtest/upload.php"} 4.13
+# HELP speedtest_target_update_duration_ms Duration of speedtest runs in seconds
+# TYPE speedtest_target_update_duration_ms gauge
+speedtest_target_update_duration_ms 0.206249213
+# HELP speedtest_test_duration_ms Duration of speedtest runs in seconds
+# TYPE speedtest_test_duration_ms gauge
+speedtest_test_duration_ms 6.257747472
+# HELP speedtest_upload_speed_mbps Latency to Speedtest Server in seconds
+# TYPE speedtest_upload_speed_mbps gauge
+speedtest_upload_speed_mbps{country="United States",distance="1.0",lat="1.0",lon="-1.0",name="Anytown, USA",server_id="1",sponsor="Dat Sponsor Doh",url="http://speedtest.example.net:8080/speedtest/upload.php"} 724.4910836862521
+*/
+
+func TestAllMetricsPopulated(t *testing.T) {
+	assert := assert.New(t)
+	require := require.New(t)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	e := New(Opts{
+		Doer: NewTestClient(),
+		Ctx:  ctx,
+	})
+	reg := prometheus.NewRegistry()
+	reg.MustRegister(e)
+
+	srv := httptest.NewServer(promhttp.HandlerFor(reg, promhttp.HandlerOpts{}))
+	defer srv.Close()
+
+	c := srv.Client()
+	resp, err := c.Get(srv.URL)
+	require.Nil(err)
+	defer resp.Body.Close()
+
+	buf, err := io.ReadAll(resp.Body)
+	require.Nil(err)
+
+	tests := []struct {
+		desc  string
+		match *regexp.Regexp
+	}{
+		{"speed_test_download_speed_desc", regexp.MustCompile(`(?m)^# HELP speedtest_download_speed_mbps .+$`)},
+		{"speed_test_download_speed", regexp.MustCompile(`(?m)^speedtest_download_speed_mbps{country=".+",distance="[0-9\.]+",lat="[0-9\.\-]+",lon="[0-9\.\-]+",name=".+",server_id="[0-9]+",sponsor=".+",url=".+"} [0-9\.]+$`)},
+	}
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			assert.True(tt.match.Match(buf), "Regex %s didn't match a line! buf: %s", tt.match.String(), string(buf))
+		})
+	}
+}
---
+++ internal/exporter/mock_responses.go
@@ -0,0 +1,126 @@
+package exporter
+
+import (
+	"bytes"
+	"io"
+	"net/http"
+	"os"
+
+	"github.com/rs/zerolog/log"
+)
+
+// RoundTripFunc .
+type roundTripFunc func(req *http.Request) *http.Response
+
+// RoundTrip .
+func (f roundTripFunc) RoundTrip(req *http.Request) (*http.Response, error) {
+	return f(req), nil
+}
+
+// NewTestClient returns *http.Client with Transport replaced to avoid making real calls
+func NewTestClient() *http.Client {
+	return &http.Client{
+		Transport: roundTripFunc(speedtestFunc),
+	}
+}
+
+func speedtestFunc(req *http.Request) *http.Response {
+	var ret *http.Response
+	switch req.URL.String() {
+	case "https://www.speedtest.net/speedtest-config.php":
+
+		ret = &http.Response{
+			StatusCode: 200,
+			// Send response to be tested
+			Body: io.NopCloser(bytes.NewBufferString(speedtestUserConfigMock)),
+			// Must be set to non-nil value or it panics
+			Header: make(http.Header),
+		}
+	case "https://www.speedtest.net/api/js/servers?engine=js&limit=10":
+		ret = &http.Response{
+			StatusCode: 200,
+			// Send response to be tested
+			Body: io.NopCloser(bytes.NewBufferString(speedtestServerListMock)),
+			// Must be set to non-nil value or it panics
+			Header:        make(http.Header),
+			ContentLength: 100,
+		}
+	case "http://speedtest.example.net:8080/speedtest/upload.php":
+		ret = &http.Response{
+			StatusCode: 200,
+			// Send response to be tested
+			Body: io.NopCloser(bytes.NewBufferString("OK")),
+			// Must be set to non-nil value or it panics
+			Header: make(http.Header),
+		}
+	case "http://speedtest.example.net:8080/speedtest/latency.txt":
+		ret = &http.Response{
+			StatusCode: 200,
+			// Send response to be tested
+			Body: io.NopCloser(bytes.NewBufferString("test=test")),
+			// Must be set to non-nil value or it panics
+			Header: make(http.Header),
+		}
+	case "http://speedtest.example.net:8080/speedtest/random2500x2500.jpg":
+		fileBytes, err := os.ReadFile("test2500.jpg")
+		if err != nil {
+			log.Error().Str("url", req.URL.String()).Err(err).Msg("Could not read file")
+		}
+		ret = &http.Response{
+			StatusCode: 200,
+			// Send response to be tested
+			Body: io.NopCloser(bytes.NewBuffer(fileBytes)),
+			// Must be set to non-nil value or it panics
+			Header: make(http.Header),
+		}
+	case "http://speedtest.example.net:8080/speedtest/random750x750.jpg":
+		fileBytes, err := os.ReadFile("test750.jpg")
+		if err != nil {
+			log.Error().Str("url", req.URL.String()).Err(err).Msg("Could not read file")
+		}
+		ret = &http.Response{
+			StatusCode: 200,
+			// Send response to be tested
+			Body: io.NopCloser(bytes.NewBuffer(fileBytes)),
+			// Must be set to non-nil value or it panics
+			Header: make(http.Header),
+		}
+	default:
+		log.Error().Str("url", req.URL.String()).Msg("Unhandled URL")
+		ret = &http.Response{
+			StatusCode: 200,
+			// Send response to be tested
+			Body: io.NopCloser(bytes.NewBufferString("Error: Unhandled URL")),
+			// Must be set to non-nil value or it panics
+			Header: make(http.Header),
+		}
+	}
+	return ret
+}
+
+var (
+	speedtestUserConfigMock = `
+	<?xml version="1.0" encoding="UTF-8"?>
+	<settings>
+	<client ip="1.2.3.4" lat="1.1" lon="-1.1" isp="Dat Sponsor Doh" isprating="1" rating="0" ispdlavg="0" ispulavg="0" loggedin="0" country="US" />
+	</settings>
+`
+
+	speedtestServerListMock = `[
+  {
+    "url": "http://speedtest.example.net:8080/speedtest/upload.php",
+    "lat": "1.00",
+    "lon": "-1.0",
+    "distance": 5,
+    "name": "Anytown, USA",
+    "country": "United States",
+    "cc": "US",
+    "sponsor": "Dat Sponsor Doh",
+    "id": "1",
+    "preferred": 0,
+    "https_functional": 1,
+    "host": "speedtest1.example.net:8080",
+    "force_ping_select": 1
+  }
+]`
+)
